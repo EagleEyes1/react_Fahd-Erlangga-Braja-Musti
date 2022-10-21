d04102acb0d1407165e852a1d7e6eb408593dcd9
fahd <fahderlangga1@gmail.com>
2022-10-21 10:38:03 +0700

    task/deploy : Pengerjaan Praktikum dan penambahan screenshot

---
+++ 19_Deployment/19_TugasPraktikum/deploy/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Website Testing Deployment
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/EagleEyes1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Saya
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
