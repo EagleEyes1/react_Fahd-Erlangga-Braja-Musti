11603a8faebf130b578017c85d9ad8435ff69a40
fahd <fahderlangga1@gmail.com>
2022-12-07 15:00:28 +0700

    task/weeklytask2: adding project

---
+++ 26_WeeklyTask2/project/src/App.js
@@ -0,0 +1,13 @@
+import Forms from './components/Form/Form';
+import Header from './components/Header/Header';
+import SetUpRouters from './routers/SetupRouters';
+
+function App() {
+  return (
+    <>
+      <SetUpRouters />
+    </>
+  );
+}
+
+export default App;
---
+++ 26_WeeklyTask2/project/src/pages/ContactUsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../components/Footer/Footer'
+import Forms from '../components/Form/Form'
+
+const ContactUsPage = () => {
+    return (
+        <>
+            <Forms />
+            <Footer />
+        </>
+    )
+}
+
+export default ContactUsPage
